4b90903e34ec13a39bdfe37e7c82c4aec8fed9d3
Alberto Reineri <reinerialberto@gmail.com>
2024-03-06 16:36:24 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+	lxappearance \
+	ranger \
+	zsh-autosuggestions \
+	zsh-syntax-highlighting \
+	neovim \
+	zsh \
+	arandr \
+	rofi \
+	snapd \
+	i3blocks \
+	apache2 \
+	mysql-server \
+	php \
+	libapache2-mod-php \
+	php-mysql \
+	phpmyadmin \
+	composer \
+	git
+
+sudo snap instll \
+	simplenote \
+	obs-studio \
+	mailspring \
+	brave \
+	dart-sass \
+	hugo \
+	tmux --classic \
+	codium --classic
+
+# Go
+curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
+
+# Mysql
+sudo mysql
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+exit
+
+# Edge
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
+sudo apt update && sudo apt install microsoft-edge-stable
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Tmuxifier
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+
+#TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Valet Linux
+composer global require cpriego/valet-linux
+export PATH=$PATH:~/.config/composer/vendor/bin
+valet install
+
+# My Projects
+mkdir ~/Progetti
+cd ~/Progetti
+valet park
+
+git clone https://github.com/albertoreineri/TypeWrite-Laravel.git
+git clone https://github.com/albertoreineri/albertoreineri.github.io.git
+
+# Fusuma
+sudo gpasswd -a $USER input
+newgrp input
+sudo apt-get install libinput-tools
+sudo apt-get install ruby
+sudo gem install fusuma
+sudo apt-get install xdotool
+mkdir -p ~/.config/fusuma        
+
+# My Dot Files
+
+#i3
+
+#i3Blocks
+
+# Dot files
